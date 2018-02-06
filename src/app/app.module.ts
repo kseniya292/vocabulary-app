702356a62b7eb9fac44c1b2f395fc843e0cad4ae
Kseniya Lifanova <kseniya.hogan@gmail.com>
2018-02-06 10:01:24 -0800

    added metaReducer function to intergrate nrgx and server side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule, TransferState, makeStateKey} from '@angular/platform-browser';
 import { NgModule, PLATFORM_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule, MatButtonModule, MatFormFieldModule, MatFormFieldControl } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
-import { environment } from '../environments/environment';
 
-// packages
-import { StoreModule, ActionsSubject } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 
 import { routesModule } from './app.routes';
 
-// application/components
+import * as fromRoot from './reducers';
+import { reducers, metaReducers } from './reducers';
+
+import { StoreModule, Store } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { DefinitionComponent } from './definition/definition.component';
 import { VocabListComponent } from './vocab-list/vocab-list.component';
 import { VocabDetailComponent } from './vocab-detail/vocab-detail.component';
-
-// application/services
 import { DefinitionService } from './definition.service';
 import { WordsService } from './words.service';
 
-import { reducers, metaReducers } from './reducers';
+export const NGRX_STATE = makeStateKey('NGRX_STATE');
 
 @NgModule({
   declarations: [
@@ -44,9 +42,42 @@ import { reducers, metaReducers } from './reducers';
     HttpClientModule,
     routesModule,
     BrowserTransferStateModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot(fromRoot.reducers, { metaReducers }),
   ],
   providers: [DefinitionService, WordsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+    public constructor(
+      private readonly transferState: TransferState,
+      private readonly store: Store<fromRoot.State>,
+  ) {
+      const isBrowser = this.transferState.hasKey<any>(NGRX_STATE);
+
+      if (isBrowser) {
+          this.onBrowser();
+      } else {
+          this.onServer();
+      }
+  }
+  onServer() {
+
+      this.transferState.onSerialize(NGRX_STATE, () => {
+          let state;
+          this.store.subscribe( ( saveState: any ) => {
+              console.log('Set for browser', JSON.stringify(saveState));
+              state = saveState;
+          }).unsubscribe();
+
+          return state;
+      });
+  }
+
+  onBrowser() {
+      const state = this.transferState.get<any>(NGRX_STATE, null);
+      this.transferState.remove(NGRX_STATE);
+      this.store.dispatch( { type: 'SET_ROOT_STATE', payload: state } );
+      console.log('Got state from server', JSON.stringify(state));
+  }
+ }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -33,24 +33,47 @@ import {
     search: fromSearch.reducer,
   };
 
-  // console.log all actions
-  export function logger(reducer: ActionReducer<State>): ActionReducer<any, any> {
-    return function(state: State, action: any): State {
-      console.log('state', state); // tslint:disable-line:no-console
-      console.log('action', action); // tslint:disable-line:no-console
+  // // console.log all actions
+  // export function logger(reducer: ActionReducer<State>): ActionReducer<any, any> {
+  //   return function(state: State, action: any): State {
+  //     console.log('state', state); // tslint:disable-line:no-console
+  //     console.log('action', action); // tslint:disable-line:no-console
 
+  //     return reducer(state, action);
+  //   };
+  // }
+
+  // make sure you export for AoT
+export function stateSetter(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function(state: any, action: any) {
+    console.log('state', state); // tslint:disable-line:no-console
+    console.log('action', action); // tslint:disable-line:no-console
+      if (action.type === 'SET_ROOT_STATE') {
+          return action.payload;
+      }
       return reducer(state, action);
-    };
-  }
+  };
+}
+
+  const _metaReducers: MetaReducer<State, any>[] = !environment.production
+  ? [stateSetter] : [];
+
+  // if ( !environment.production ) {
+  //     _metaReducers.push( debugMetaReducer );
+  // }
+
+  export const metaReducers = _metaReducers;
+
+
 
   /**
    * By default, @ngrx/store uses combineReducers with the reducer map to compose
    * the root meta-reducer. To add more meta-reducers, provide an array of meta-reducers
    * that will be composed to form the root meta-reducer.
    */
-  export const metaReducers: MetaReducer<any, any>[] = !environment.production
-    ? [logger]
-    : [];
+  // export const metaReducers: MetaReducer<any, any>[] = !environment.production
+  //   ? [logger]
+  //   : [];
 
   /**
    * App Reducer Selectors
